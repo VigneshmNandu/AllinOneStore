3ed8bdc2c5f653473f35ebf3153dec39f7c7968d
DonVigu <vigneshmvigi@gmail.com>
2021-04-27 17:39:10 +0530

    trying to fix static file error while deploying

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -1,7 +1,16 @@
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
+
+# application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djecommerce.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
