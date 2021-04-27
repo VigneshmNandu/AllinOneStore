e390ad1de5dcee33c57d71a0fa16b00e0d379d71
DonVigu <vigneshmvigi@gmail.com>
2021-04-27 18:04:41 +0530

    trying to fix deployement error | modified whitenoise

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -7,10 +7,10 @@
 # application = get_wsgi_application()
 
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djecommerce.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
